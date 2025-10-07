1a585a91b63ab84af90521b19f1698e9ec43e017
apark07 <andrew1207park@gmail.com>
2025-10-06 23:48:10 -0400

    added scroll feature

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,61 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers, required this.onRestart});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+    required this.onRestart,
+  });
   final List<String> chosenAnswers;
   final void Function() onRestart;
-  
 
+  List<Map<String, Object>> getSummaryData() {
+    List<Map<String, Object>> summary = [];
 
-  List<Map<String,Object>> getSummaryData(){
-    List<Map<String,Object>> summary = [];
-
-    for(var i =0; i<chosenAnswers.length; i++)
-    {
-      summary.add(
-        {
-          'question_index': i,
-          'question': questions[i].text,
-          'correct_answer': questions[i].answers[0],
-          'user_answer': chosenAnswers[i],
-        });
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
     }
     return summary;
-
   }
+
   @override
   Widget build(context) {
-
     final summaryData = getSummaryData();
     final numCorrectAnswers = summaryData.where((data) {
       return data['user_answer'] == data['correct_answer'];
     }).length;
     final totalQuestions = questions.length;
-    
+
     return SizedBox(
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-             'You answered $numCorrectAnswers out of $totalQuestions questions correctly!',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'You answered $numCorrectAnswers out of $totalQuestions questions correctly!',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
               ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text('List of Answers and Question'),
-            //'Loop' through chosenAnswers and do something with it
-            const SizedBox(
-              height: 30,
-            ),
-            TextButton(
-              onPressed: onRestart,
-              child: const Text('Restart Quiz!'),
-            ),
-          ],
+              const SizedBox(height: 30),
+              QuestionSummary(summaryData: summaryData),
+
+              const SizedBox(height: 30),
+              TextButton.icon(
+                onPressed: onRestart,
+                icon: const Icon(Icons.restart_alt),
+                label: const Text('Restart Quiz!'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
